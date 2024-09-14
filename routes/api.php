5e0fd659422b5b0cccc89f3e9ca554ca3954a273
Arkar <arkarchanmyaeko@gmail.com>
2024-09-14 15:32:00 +0700

    feat(blog): fetch blog with user and category feat(comment): fetch comments by blog_id

--- routes/api.php
+++ routes/api.php
@@ -17,32 +17,43 @@ use App\Http\Controllers\CommentController;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(["auth:sanctum"])->get("/user", function (Request $request) {
+  return $request->user();
 });
 
 // blog table
 
 //public route
-Route::get('blogs/all', [BlogController::class, 'all']);
+Route::get("/blogs/all", [BlogController::class, "all"]);
+Route::get("/blogs/popular", [BlogController::class, "latest"]);
+Route::get("/blogs/{id}", [BlogController::class, "show"]);
+
+
+Route::get("/comments/{blog_id}", [CommentController::class, "all"]);
 
 //private  rotues
-Route::prefix('blogs')->middleware('auth:sanctum')->controller(BlogController::class)->group(function () {
-    Route::get('/all', 'all');
-    Route::post('/store', 'store');
-    Route::put('/edit/{id}', 'edit');
-    Route::delete('/delete/{id}', 'delete');
-    Route::post('/{id}/subscription', 'subscription');
+Route::prefix("blogs")
+  ->middleware("auth:sanctum")
+  ->controller(BlogController::class)
+  ->group(function () {
+    // Route::get('/all', 'all');
+    Route::post("/store", "store");
+    Route::put("/edit/{id}", "edit");
+    Route::delete("/delete/{id}", "delete");
+    Route::post("/{id}/subscription", "subscription");
     // Route::post('/{id}/comment/store', 'storeComment');
     // Route::put('/comment/edit/{id}', 'updateComment');
     // Route::delete('/comment/delete/{id}', 'deleteComment');
-});
+  });
 
 //e.o blog table
 
-// comment table 
-Route::prefix('comments')->middleware('auth:sanctum')->controller(CommentController::class)->group(function () {
-    Route::post('{blogId}/store', 'store');
-    Route::put('/edit/{id}', 'edit');
-    Route::delete('/delete/{id}', 'delete');
-});
+// comment table
+Route::prefix("comments")
+  ->middleware("auth:sanctum")
+  ->controller(CommentController::class)
+  ->group(function () {
+    Route::post("{blogId}/store", "store");
+    Route::put("/edit/{id}", "edit");
+    Route::delete("/delete/{id}", "delete");
+  });
