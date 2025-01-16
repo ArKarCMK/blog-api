8b1c1b77b85d315bd53297ca9acdfcdc8f29763d
Arkar <arkarchanmyaeko@gmail.com>
2025-01-16 14:15:45 +0700

    feat(blog): remove intro column in blog table

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
-    protected $fillable = ['user_id', 'category_id', 'title', 'slug', 'intro', 'body'];
+    protected $fillable = ['user_id', 'category_id', 'title', 'slug',  'body'];
 
     public function user()
     {
